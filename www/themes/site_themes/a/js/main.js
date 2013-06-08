60d75d22ec45c032ba832548bf951edd60d2ddf1
Sean Gravener <sean@gravener.net>
2013-06-08 11:46:19 -0400

    work in progress

--- www/themes/site_themes/a/js/main.js
+++ www/themes/site_themes/a/js/main.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function($) {
 
 	$('.flexslider').flexslider({
 	   animation: "slide",
+	   controlNav: false,
 	   // controlNav: "thumbnails",
 	   // controlsContainer: ".flex-container",
 	   pauseOnHover: true,
