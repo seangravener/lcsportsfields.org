327482129fcebccad10f94e14ba1bae210796a09
Sean Gravener <sean@gravener.net>
2013-06-13 23:24:25 -0400

    wip portable menu

--- www/themes/site_themes/a/js/main.js
+++ www/themes/site_themes/a/js/main.js
@@ -1,6 +1,4 @@
-
-
-jQuery(document).ready(function($) { 
+jQuery( document ).ready( function($) { 
 
 	$('.flexslider').flexslider({
 	   animation: "slide",
@@ -11,4 +9,63 @@ jQuery(document).ready(function($) {
 	   touch: true,
 	});
 
+	var navTopPortable = $( ".nav--top-portable" ),
+		navPortable = $( ".nav--portable" ),
+		navMobileMenuSelector = $("#mobile__menu-selector");
+
+	
+
+	var isPortable = function () {
+		if ( navTopPortable.css( "display" ) === "block" ) {
+			return true;
+		};
+	}
+
+	var mobileMenu = function () {
+		if ( isPortable() ) {
+
+			console.log('moblemen');
+			// var navPortable = $(".nav--portable");
+
+			navPortable.on( "click", "> li > a", function( event ) {
+				var $this = $(this),
+					navDropdownContainer = $this.next('.nav--dropdown--menu');
+
+				navDropdownContainer.toggle();
+		
+				// prevent default action if contains children
+				if ( navDropdownContainer.length > 0 ) {
+					event.preventDefault();
+				}
+
+			});
+
+		} else {
+			navPortable.off( "click", "**" );
+		}
+
+
+		navMobileMenuSelector.on( "click", function( event ) {
+			var $this = $(this);
+
+			$this.parent('li').toggleClass('active');
+
+			navPortable.toggle();
+
+			event.preventDefault();
+
+		});
+
+		
+	}
+
+	// mobile nav in use on page load?
+		mobileMenu();
+
+	// recheck if viewport is resized
+	$( window ).resize( function() {
+			mobileMenu();
+	});
+
+	
 });
