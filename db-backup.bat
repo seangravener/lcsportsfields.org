126a41626aceda9392d388a9b47467a8e8b58765
Sean Gravener <sean@gravener.net>
2013-06-23 00:00:08 -0400

    work in progress

--- db-backup.bat
+++ db-backup.bat
@@ -3,4 +3,4 @@ set d=%d: =_%
 set t=%time:~0,2%%time:~3,2%%time:~6,2%
 set t=%t: =0%
 
-C:\xampp\mysql\bin\mysqldump -u root lcsfa_dev > build\db\lcsfa_dev_snapshot--%d%-%t%.sql
+C:\xampp\mysql\bin\mysqldump -u root lcsfa_dev > db\lcsfa_dev_snapshot--%d%-%t%.sql
